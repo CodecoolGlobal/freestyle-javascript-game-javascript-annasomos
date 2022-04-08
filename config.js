9d644fecfe2701ad4e01076f4d57e851d13a3a42
annasomos <annasomoss@gmail.com>
2022-04-08 07:45:32 +0200

    Add 4 new puppies to img

--- config.js
+++ config.js
@@ -17,7 +17,13 @@ export const config = {
         'img/puppy7.png',
         'img/puppy8.png',
         'img/puppy9.png',
-        'img/puppy10.png'
+        'img/puppy10.png',
+        'img/puppy11.png',
+        'img/puppy12.png',
+        'img/puppy13.png',
+        'img/puppy14.png',
+        'img/puppy15.png',
+        'img/puppy16.png'
 
     ],
     hideTimeOut : 1000,
